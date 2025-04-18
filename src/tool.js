a3ca839faaf54241bf9fcb3da25b58bc8826bede
ORNG <acodrst@mail.com>
2025-04-18 10:42:20 -0700

    ver

--- src/tool.js
+++ src/tool.js
@@ -1,13 +1,11 @@
-const emoji = "🚷";
-const domain = "noidentity.dev";
-const backup = Deno.env.get("CL_NID_BACKUP");
 import { create, web_deal } from "fpng-sign-serve";
+const backup = Deno.env.get("CL_NID_BACKUP");
 const site = {};
 site.page = Deno.readTextFileSync("assets/page.html");
 site.css = Deno.readTextFileSync("assets/style.css");
-create(site,domain,backup,emoji)
+create(site,backup)
 Deno.serve({
   port: 3052,
   hostname: "0.0.0.0",
-  handler: (req) => web_deal(req,domain),
+  handler: (req) => web_deal(req),
 });
