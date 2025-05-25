084a1f43bf6fb55a34ed341895f1c9986c8aad0d
ORNG <acodrst@mail.com>
2025-05-25 16:26:09 -0700

    nostr verify

--- src/app.js
+++ src/app.js
@@ -1,38 +1,64 @@
+import { model_to_dots } from "text-model-dot";
+import mqtt from "mqtt/dist/mqtt.esm";
+import { gsdot_svg } from "gsdot-svg";
+import fileSaver from "file-saver";
+import { verifyEvent } from "nostr-tools/pure";
 document.body.insertAdjacentHTML("beforeend", site.page);
 const style = document.createElement("style");
 style.textContent = site.css;
 document.head.appendChild(style);
-document.getElementById("mqtt_text").value=localStorage.getItem('mqtt_broker') || 'wss://broker.emqx.io:8084/mqtt'
-localStorage.setItem('mqtt_broker',document.getElementById("mqtt_text").value)
-document.getElementById("topic_text").value=localStorage.getItem('topic') || self.crypto.randomUUID();
-localStorage.setItem('topic',document.getElementById("topic_text").value)
-const client = mqtt.connect(localStorage.getItem('mqtt_broker'),{clean:true,clientId:localStorage.getItem('client_id'),connectTimeout:1000});
+document.getElementById("mqtt_text").value =
+  localStorage.getItem("mqtt_broker") || "wss://broker.emqx.io:8084/mqtt";
+localStorage.setItem("mqtt_broker", document.getElementById("mqtt_text").value);
+document.getElementById("topic_text").value = localStorage.getItem("topic") ||
+  self.crypto.randomUUID();
+localStorage.setItem("topic", document.getElementById("topic_text").value);
+const client = mqtt.connect(localStorage.getItem("mqtt_broker"), {
+  clean: true,
+  clientId: localStorage.getItem("client_id"),
+  connectTimeout: 1000,
+});
 client.on("connect", function () {
-  console.log(`Connected to:${localStorage.getItem('mqtt_broker')}`);
-  client.subscribe(localStorage.getItem('topic'), function (err) {
+  console.log(`Connected to:${localStorage.getItem("mqtt_broker")}`);
+  client.subscribe(localStorage.getItem("topic"), function (err) {
     if (!err) {
     }
   });
 });
-const cur_map = localStorage.getItem("gs_map_Top")||`:: level\nTop\n
+const cur_map = localStorage.getItem("gs_map_Top") || `:: level\nTop\n
 :: processes
 Find Water Location
 :: back
-Human/Cultural Memory`
-localStorage.setItem('gs_map_Top',cur_map)
-client.on('message', function (topic, message) {
-  localStorage.setItem('gs_map_Top',`${localStorage.getItem('gs_map_Top').trim()}\n${message.toString()}`)
-    update_top('map') 
-})
-import mqtt from "mqtt/dist/mqtt.esm";
-import { model_to_dots } from "text-model-dot";
-import { gsdot_svg } from "gsdot-svg";
-import fileSaver from "file-saver";
+Human/Cultural Memory`;
+localStorage.setItem("gs_map_Top", cur_map);
+client.on("message", function (topic, message) {
+  const mess_string = message.toString();
+  if (mess_string.match(/^\{.+\}$/)) {
+    const mess = JSON.parse(mess_string);
+    if (verifyEvent(mess)) {
+      localStorage.setItem(
+        "gs_map_Top",
+        `${localStorage.getItem("gs_map_Top").trim()}\n${
+          JSON.parse(mess_string).content
+        }`,
+      );
+    } else {
+      localStorage.setItem(
+        "gs_map_Top",
+        ":: level\nTop\n:: processes\nInvalid message",
+      );
+    }
+  } else {localStorage.setItem(
+      "gs_map_Top",
+      `${localStorage.getItem("gs_map_Top").trim()}\n${mess_string}`,
+    );}
+  update_top("map");
+});
 function update_top(kind) {
-  const dots = model_to_dots(localStorage.getItem("gs_map_Top"), false).dots;
-    gsdot_svg(dots["Top"], "default", kind).then((svg) =>
-      localStorage.setItem("svg_content", svg)
-    );
+  const levs = model_to_dots(localStorage.getItem("gs_map_Top"), false);
+  gsdot_svg(levs["Top"].dots, "default", kind, "Top", levs).then((svg) =>
+    localStorage.setItem("svg_content", svg)
+  );
 }
 document.getElementById("save").addEventListener("click", (e) => {
   if (
@@ -64,31 +90,23 @@ td{border: 2px solid; padding:7px;font-family: monospace;font-size:20px;}</style
     if (!(save_name === null)) saveAs(blob_content, save_name);
   }
 });
+function select(d) {
+  for (let i of ["map", "key", "legal"]) {
+    if (i == d) document.getElementById(i).style.display = "block";
+    else document.getElementById(i).style.display = "none";
+  }
+}
 update_top("map");
 document.getElementById("key_toggle").addEventListener("click", () => {
-  document.getElementById("legal").style.display = "none";
-  if (
-    document.getElementById("key").style.display == "none" ||
-    document.getElementById("key").style.display == ""
-  ) {
-    document.getElementById("key").style.display = "block";
-    document.getElementById("map").style.display = "none";
-    update_top("key");
-  } else {
-    document.getElementById("key").style.display = "none";
-    document.getElementById("map").style.display = "block";
-    update_top("map");
-  }
+  select("key");
+  update_top("key");
 });
 document.getElementById("home").addEventListener("click", () => {
-  document.getElementById("key").style.display = "none";
-  document.getElementById("map").style.display = "block";
-  document.getElementById("legal").style.display = "none";
+  select("map");
+  update_top("map");
 });
 document.getElementById("legal_view").addEventListener("click", () => {
-  document.getElementById("key").style.display = "none";
-  document.getElementById("map").style.display = "none";
-  document.getElementById("legal").style.display = "block";
+  select("legal");
 });
 document.getElementById("export").addEventListener("click", () => {
   const save_name = prompt(
@@ -100,18 +118,41 @@ document.getElementById("export").addEventListener("click", () => {
   });
   if (!(save_name === null)) saveAs(blob, save_name);
 });
-document.getElementById("gs_text").addEventListener("keypress", (e) => {
-  if (e.key=="Enter"){
-      client.publish(localStorage.getItem('topic'), e.target.value.trim());
+document.getElementById("gs_text").addEventListener("input", async (e) => {
+  if (e.data) {
+    if (e.data.includes("`")) {
+      const ss = e.target.selectionStart;
+      const val = e.target.value.slice(0, ss - 1) + e.target.value.slice(ss);
+      e.target.value = val;
+      e.target.selectionEnd = ss - 1;
+      if (globalThis.nostr) {
+        const pub = await globalThis.nostr.getPublicKey();
+        const signed_event = await globalThis.nostr.signEvent({
+          "pubkey": pub,
+          "created_at": Math.floor(Date.now() / 1000),
+          "kind": 1,
+          "tags": [],
+          "content": val.trim(),
+        });
+        client.publish(
+          localStorage.getItem("topic"),
+          JSON.stringify(signed_event),
+        );
+      } else {
+        client.publish(localStorage.getItem("topic"), val.trim());
+      }
+    }
   }
 });
 document.getElementById("mqtt_text").addEventListener("input", (e) => {
-  localStorage.setItem('mqtt_broker',e.target.value)
+  localStorage.setItem("mqtt_broker", e.target.value);
 });
 document.getElementById("topic_text").addEventListener("input", (e) => {
-  localStorage.setItem('topic',e.target.value)
+  localStorage.setItem("topic", e.target.value);
 });
-document.getElementById("import").addEventListener("click", (e) => { document.getElementById("file").click() })
+document.getElementById("import").addEventListener("click", (e) => {
+  document.getElementById("file").click();
+});
 document.getElementById("file").addEventListener("change", (e) => {
   const file = e.target.files[0];
   const reader = new FileReader();
